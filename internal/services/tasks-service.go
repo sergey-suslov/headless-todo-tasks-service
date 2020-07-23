5eb4c8585e2d910f001301223ab432d7089bf790
Sergey Suslov <sergey.suslov.nsk@gmail.com>
2020-07-23 22:38:21 +0700

    test: add tasks service error tests

--- internal/services/tasks-service.go
+++ internal/services/tasks-service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"errors"
 	"headless-todo-tasks-service/internal/entities"
 	"headless-todo-tasks-service/internal/services/repositories"
 )
@@ -15,5 +16,14 @@ func NewTasksService(tasksRepository repositories.TasksRepository) *TasksService
 }
 
 func (service *TasksService) Create(ctx context.Context, name, description, userId string) (*entities.Task, error) {
+	if name == "" {
+		return nil, errors.New("name must be present")
+	}
+	if description == "" {
+		return nil, errors.New("description must be present")
+	}
+	if userId == "" {
+		return nil, errors.New("userId must be present")
+	}
 	return service.tasksRepository.Create(ctx, name, description, userId)
 }
--- internal/services/tasks-service_test.go
+++ internal/services/tasks-service_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -15,7 +16,54 @@ type MockedTasksRepository struct {
 
 func (m *MockedTasksRepository) Create(ctx context.Context, name, description, userId string) (*entities.Task, error) {
 	args := m.Called(ctx, name, description, userId)
-	return args.Get(0).(*entities.Task), nil
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entities.Task), args.Error(1)
+}
+
+func TestTasksService_Create_Error(t *testing.T) {
+	t.Run("Empty name", func(t *testing.T) {
+		mockedTaskRepository := new(MockedTasksRepository)
+		testError := errors.New("name must be present")
+		service := &TasksService{mockedTaskRepository}
+		task, err := service.Create(context.Background(), "", "description", "1")
+
+		mockedTaskRepository.AssertNotCalled(t, "Create", context.Background(), "", "description", "1")
+		assert.Nil(t, task)
+		if assert.Error(t, err) {
+			assert.EqualError(t, err, testError.Error())
+		}
+		mockedTaskRepository.AssertExpectations(t)
+	})
+
+	t.Run("Empty description", func(t *testing.T) {
+		mockedTaskRepository := new(MockedTasksRepository)
+		testError := errors.New("description must be present")
+		service := &TasksService{mockedTaskRepository}
+		task, err := service.Create(context.Background(), "name", "", "1")
+
+		mockedTaskRepository.AssertNotCalled(t, "Create", context.Background(), "name", "", "1")
+		assert.Nil(t, task)
+		if assert.Error(t, err) {
+			assert.EqualError(t, err, testError.Error())
+		}
+		mockedTaskRepository.AssertExpectations(t)
+	})
+
+	t.Run("Empty userId", func(t *testing.T) {
+		mockedTaskRepository := new(MockedTasksRepository)
+		testError := errors.New("userId must be present")
+		service := &TasksService{mockedTaskRepository}
+		task, err := service.Create(context.Background(), "name", "description", "")
+
+		mockedTaskRepository.AssertNotCalled(t, "Create", context.Background(), "name", "description", "")
+		assert.Nil(t, task)
+		if assert.Error(t, err) {
+			assert.EqualError(t, err, testError.Error())
+		}
+		mockedTaskRepository.AssertExpectations(t)
+	})
 }
 
 func TestTasksService_Create_Success(t *testing.T) {
@@ -26,11 +74,12 @@ func TestTasksService_Create_Success(t *testing.T) {
 		Description: "description 1",
 		UserId:      "2",
 	}
-	mockedTaskRepository.On("Create", context.Background(), testTask.Name, testTask.Description, testTask.UserId).Return(testTask)
+	mockedTaskRepository.On("Create", context.Background(), testTask.Name, testTask.Description, testTask.UserId).Return(testTask, nil)
 	service := &TasksService{mockedTaskRepository}
 
 	task, _ := service.Create(context.Background(), testTask.Name, testTask.Description, testTask.UserId)
 
 	mockedTaskRepository.AssertCalled(t, "Create", context.Background(), testTask.Name, testTask.Description, testTask.UserId)
+	mockedTaskRepository.AssertExpectations(t)
 	assert.Equal(t, task, testTask)
 }
